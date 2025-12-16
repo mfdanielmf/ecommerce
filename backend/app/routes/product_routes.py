0bfb9c8526237134428063ff2887cfc635ce5559
Daniel Martínez Ferández <mfdanielmf@gmail.com>
2025-12-16 20:32:15 +0100

    feat: habilitar cors

--- backend/app/routes/product_routes.py
+++ backend/app/routes/product_routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, jsonify
+from flask_cors import cross_origin
 
 from app.models.producto import Producto
 from app.services.product_services import obtener_todos_los_productos
@@ -7,6 +8,7 @@ producto_bp = Blueprint("productos", __name__)
 
 
 @producto_bp.route("/")
+@cross_origin()
 def productos():
     productos: list[Producto] = obtener_todos_los_productos()
 
