706ad7eed703be229840270d994ee1560fe507f9
Daniel Martínez Fernández <mfdanielmf@gmail.com>
2025-12-17 03:05:15 +0100

    feat: implementar obtención de productos por id

--- frontend/src/api/productos.js
+++ frontend/src/api/productos.js
@@ -1,12 +1,21 @@
-import axios from "axios"
+import axios from 'axios'
 
 export async function getProductos() {
-  try{
+  try {
     const req = await axios.get('http://127.0.0.1:8080/api/productos/')
 
     return req.data
-  }catch(e){
-    console.error("ERROR:", e)
+  } catch (e) {
+    console.error('ERROR:', e)
   }
+}
 
+export async function getProductoId(id) {
+  try {
+    const req = await axios.get(`http://127.0.0.1:8080/api/productos/${id}`)
+
+    return req.data
+  } catch (e) {
+    console.error('ERROR:', e)
+  }
 }
--- frontend/src/stores/productosStore.js
+++ frontend/src/stores/productosStore.js
@@ -1,4 +1,4 @@
-import { getProductos } from '@/api/productos'
+import { getProductoId, getProductos } from '@/api/productos'
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
@@ -6,7 +6,9 @@ export const productStore = defineStore('productos', () => {
   const productos = ref([])
   const error = ref(null)
   const cargando = ref(false)
+  const productoId = ref(null)
 
+  //TODOS
   async function fetchProductos() {
     if (productos.value.length) return
 
@@ -14,12 +16,37 @@ export const productStore = defineStore('productos', () => {
     try {
       const productosFetch = await getProductos()
       productos.value = productosFetch.products
-    } catch (e) {
-      error.value = e.message
+    } catch {
+      error.value = 'Error al obtener la lista de productos'
     } finally {
       cargando.value = false
     }
   }
 
-  return { productos, error, fetchProductos, cargando }
+  //FILTRAR
+  function filtrarPorId(id) {
+    return productos.value.find((productoActual) => productoActual.id === Number(id))
+  }
+
+  async function fetchProductoId(id) {
+    const productoActual = filtrarPorId(id)
+
+    if (productoActual) {
+      productoId.value = productoActual
+
+      return
+    }
+
+    cargando.value = true
+    try {
+      const productoFetch = await getProductoId(id)
+      productoId.value = productoFetch.producto
+    } catch {
+      error.value = `Error al obtener el producto con ID ${id}`
+    } finally {
+      cargando.value = false
+    }
+  }
+
+  return { productos, productoId, error, cargando, fetchProductos, fetchProductoId, filtrarPorId }
 })
