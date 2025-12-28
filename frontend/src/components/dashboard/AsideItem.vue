1e25afc5597d1d254d345776615326e9f0a77e59
Daniel Martínez Fernández <mfdanielmf@gmail.com>
2025-12-28 17:02:04 +0100

    feat: añadir estructura visual aside dashboard

---
+++ frontend/src/components/dashboard/AsideItem.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+  icon: {
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <li>
+    <button class="is-drawer-close:tooltip is-drawer-close:tooltip-right" :data-tip="label">
+      <component :is="icon" :size="16" />
+      <span class="is-drawer-close:hidden">{{ label }}</span>
+    </button>
+  </li>
+</template>
--- frontend/src/layouts/DashboardLayout.vue
+++ frontend/src/layouts/DashboardLayout.vue
@@ -1,7 +1,49 @@
 <script setup>
+import AsideItem from '@/components/dashboard/AsideItem.vue'
+import {
+  FolderTreeIcon,
+  LayoutDashboardIcon,
+  PackageIcon,
+  PanelLeftOpenIcon,
+  ShoppingCartIcon,
+} from 'lucide-vue-next'
+
 // ------------ACORDARME DE IMPLEMENTAR LÓGICA PARA NO PERMITIR EL ACCESO AL DASH MIENTRAS NO SE CARGUE EL USUARIO Y NO SEA ADMIN!!!!!!!!!! (PONDRÉ UN SPINNER O ALGO ASÍ. YA VERÉ) ---------------------
 </script>
 
 <template>
-  <h1>test</h1>
+  <div class="drawer lg:drawer-open">
+    <input id="my-drawer-4" type="checkbox" class="drawer-toggle" />
+    <div class="drawer-content">
+      <!-- Navbar -->
+      <nav class="navbar w-full bg-base-300">
+        <label for="my-drawer-4" aria-label="open sidebar" class="btn btn-square btn-ghost">
+          <PanelLeftOpenIcon :size="24" />
+        </label>
+
+        <div class="px-4">Test</div>
+      </nav>
+
+      <!-- Contenido principal -->
+      <div class="p-4">Test</div>
+    </div>
+
+    <div class="drawer-side is-drawer-close:overflow-visible">
+      <label for="my-drawer-4" aria-label="close sidebar" class="drawer-overlay"></label>
+      <div
+        class="flex min-h-full flex-col items-start bg-base-200 is-drawer-close:w-14 is-drawer-open:w-64"
+      >
+        <!-- Contenido sidebar -->
+        <ul class="menu w-full grow">
+          <AsideItem :icon="LayoutDashboardIcon" label="Dashboard" />
+
+          <AsideItem :icon="PackageIcon" label="Productos" />
+
+          <AsideItem :icon="FolderTreeIcon" label="Categorías" />
+
+          <AsideItem :icon="ShoppingCartIcon" label="Pedidos" />
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
