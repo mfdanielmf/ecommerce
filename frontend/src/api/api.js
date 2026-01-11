92d72de4d5b78b0341a85d0df97ab22c3e35432a
Daniel Martínez Fernández <mfdanielmf@gmail.com>
2026-01-11 21:41:21 +0100

    feat: añadir interceptor para mandar token csrf

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,22 @@
 import axios from 'axios'
+import { obtenerTokenCSRF } from './csrf'
 
 export default (url = 'http://localhost:8080') => {
-  return axios.create({
+  const api = axios.create({
     baseURL: url,
   })
+
+  // Mandamos token csrf si el endpoint tiene jwt_required
+  //El token jwt no hace falta (ya se manda por las cookies al tener withCredentials true, pero el csrf no)
+  api.interceptors.request.use((config) => {
+    const csrfToken = obtenerTokenCSRF()
+
+    if (csrfToken && ['post', 'put', 'delete'].includes(config.method)) {
+      config.headers['X-CSRF-TOKEN'] = csrfToken
+    }
+
+    return config
+  })
+
+  return api
 }
--- frontend/src/api/categoriasApi.js
+++ frontend/src/api/categoriasApi.js
@@ -5,12 +5,12 @@ export default {
     return api().get('/api/categorias/')
   },
   insertarCategoria(data) {
-    return api().post('/api/categorias/', data)
+    return api().post('/api/categorias/', data, { withCredentials: true })
   },
   eliminarCategoria(id) {
-    return api().delete(`/api/categorias/${id}`)
+    return api().delete(`/api/categorias/${id}`, { withCredentials: true })
   },
   editarCategoria(id, data) {
-    return api().put(`/api/categorias/${id}`, data)
+    return api().put(`/api/categorias/${id}`, data, { withCredentials: true })
   },
 }
